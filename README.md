034ab329811a27e11dd40d54fd1784d668f8e22e
alen <alen@touzi.org>
2017-08-04 10:12:49 +0800

    update README.md

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ cmake -DCMARK_TESTS=OFF ../cmark
 make 
 cd ..
 #设置编译时动态库查找路径
-export LIBRARY_PATH=./build/src:./build/extensions:$LIBRARY_PATH
+export LIBRARY_PATH=./build:./:$LIBRARY_PATH
 #设置运行时动态库查找路径
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
 #构建release版
--- build.sh
+++ build.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-if [ -f build ] ; then
-	mkdir build
+[ ! -d build ] && mkdir build
+
+if [ -d build ] ; then
 	cd build
-	cmake -DCMARK_TESTS=OFF ../cmark
+	cmake -DCMARK_TESTS=OFF ..
 	make -j4
 	cd ..
+	cp build/libcmark-wrapper.so .
+
+	# for link
+	export LIBRARY_PATH=./:$LIBRARY_PATH
+	# for run
+	export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+
+	case "$#" in
+		0)
+			[ ! -f target/debug ] && mkdir -p target/debug 
+			cp build/libcmark-wrapper.so target/debug
+			cargo build
+		;;
+		1)
+			if [ "%{1}" = "--release" ] ; then
+				[ ! -f target/release ] && mkdir -p target/release
+				cp build/libcmark-wrapper.so target/release
+			else 
+				[ ! -f target/debug ] && mkdir -p target/debug
+				cp build/libcmark-wrapper.so target/debug
+			fi
+			cargo build ${1}
+		;;
+	esac
+else
+	echo "build cmark failed"
+	exit 1;
 fi
